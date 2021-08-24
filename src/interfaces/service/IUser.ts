590af8f20a3e2047a0ed6bad95b011de91ecfb3b
joshuaebowling <joshuaebowling@gmail.com>
2021-08-24 14:39:02 -0400

    JBOT-8 create new user service

---
+++ src/interfaces/service/IUser.ts
@@ -0,0 +1,8 @@
+import { IResponse } from "../../interfaces/IResponse"
+
+export interface IUser {
+    checkUsernameUnique: (username: string) => Promise<IResponse<boolean>>
+    createUser: (username: string, publicKey: string ) => Promise<IResponse<number>>
+    getPublicKey: (username: string) => Promise<IResponse<string>>
+    login: (username: string, challenge: string) => Promise<IResponse<boolean>>
+}
