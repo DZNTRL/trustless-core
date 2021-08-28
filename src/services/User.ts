03942978e8a709f7f6d101dc3dd548573f69fc3e
joshuaebowling <joshuaebowling@gmail.com>
2021-08-28 10:54:59 -0400

    JBOT-26 #resolve add lofi DI

--- src/services/User.ts
+++ src/services/User.ts
@@ -1,5 +1,14 @@
 import { IUser } from "../interfaces/service/IUser"
+import { IUser as IUserRepo } from "../interfaces/repo/IUser"
+import mysql from "mysql2/promise"
+
+import { defaultInjections as ioc } from "../ioc"
 export class User implements IUser {
+    repo: IUserRepo
+    constructor(pool: mysql.Pool) {
+        this.repo = ioc().UserRepo(pool)
+    }
     checkUsernameUnique(username) {
+        return this.repo.checkUsernameUnique(username)
     }
 }
