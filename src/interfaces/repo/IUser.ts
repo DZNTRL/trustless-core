97276c03418467be1c1cacd2e63a8a80dd2ab477
joshuaebowling <joshuaebowling@gmail.com>
2021-08-28 20:07:03 -0400

    JBOT-8 finish rev1 of user service

--- src/interfaces/repo/IUser.ts
+++ src/interfaces/repo/IUser.ts
@@ -4,7 +4,7 @@ export interface IUser {
     checkUsernameUnique: (username: string) => Promise<IResponse<boolean>>
     createUser: (username: string, publicKey: string ) => Promise<IResponse<number>>
     getPublicKey: (username: string) => Promise<IResponse<string>>
-    createChallenge: (challenge: string, username: string) => Promise<IResponse<boolean>>
+    createChallenge: (username: string, challenge: string) => Promise<IResponse<boolean>>
     getChallenge: (username: string) => Promise<IResponse<string>>
     verifyChallenge: (username: string, challenge: string) => Promise<IResponse<boolean>>
     clearChallenge: (username: string) => Promise<IResponse<boolean>>
--- src/services/User.ts
+++ src/services/User.ts
@@ -1,8 +1,11 @@
 import { IUser } from "../interfaces/service/IUser"
 import { IUser as IUserRepo } from "../interfaces/repo/IUser"
 import mysql from "mysql2/promise"
+import { Response } from "../models/Response"
+import { createChallenge } from "../utils/challenger"
 
 import { defaultInjections as ioc } from "../ioc"
+import { ResponseMessages } from "../enums/ResponseMessages"
 export class User implements IUser {
     repo: IUserRepo
     constructor(pool: mysql.Pool) {
@@ -10,5 +13,38 @@ export class User implements IUser {
     }
     checkUsernameUnique(username) {
         return this.repo.checkUsernameUnique(username)
+    }
+    createUser(username, publicKey) {
+        return this.repo.createUser(username, publicKey)
+    }
+    getPublicKey(username) {
+        return this.repo.getPublicKey(username)
+    }
+    async requestLogin(username) {
+        const resp = new Response<string>();
+        const getResp = await this.repo.getChallenge(username)
+        if(getResp.Data === null) {
+            var userResp = await this.repo.checkUsernameUnique(username)
+            // if the username is unique, then return without creating challenge
+            if(userResp.Data) {
+                resp.Message = ResponseMessages.NotFound.toString()
+                resp.Data = null
+                return resp                
+            }
+            let challenge = createChallenge()
+            let createResp = await this.repo.createChallenge(challenge, username)
+            if(!createResp.Data) {
+                resp.IsError = true
+                resp.Message = createResp.Message
+            } else {
+                resp.Data = challenge
+            }
+        } else {
+            resp.Data = getResp.Data
+        }
+        return resp
+    }
+    login(username, challenge) {
+        return this.repo.verifyChallenge(username, challenge)
     }
 }
